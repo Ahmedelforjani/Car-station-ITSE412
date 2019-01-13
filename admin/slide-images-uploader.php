f24bd54c30653c6e42d63d7a3a6fb82faa165776
basherbz <basherbz@users.noreply.github.com>
2019-01-13 07:50:18 +0200

    Revert "Revert "Home page backend""
    This reverts commit ffeeac43b59cd43672a2acc9b787d94a9e9fcdc1.

--- admin/slide-images-uploader.php
+++ admin/slide-images-uploader.php
@@ -1,5 +1,5 @@
 <?php
-
+  include "../DB_Connect.php";
   // print_r($_FILES['images']);
 
   if(isset($_FILES['images'])) {
@@ -8,6 +8,17 @@
     if( !empty($image['name']) ) {
       $imageName = rand(0, 100000) . "_" . $image['name'];
       move_uploaded_file($image['tmp_name'], "images/Slider/" . $imageName);
+
+      $query = "INSERT INTO slide(img) VALUES(:img_url)";
+
+      $stmt = $con-> prepare($query);
+      $stmt->bindParam(':img_url', $imageName);
+        if($stmt->execute()) {
+          echo "succes";
+        } else {
+          echo "failed";
+        }
+
     } else {
       $imageName = "default.png";
     }
