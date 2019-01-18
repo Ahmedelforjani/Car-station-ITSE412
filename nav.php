6785eb218ee2d9ef512fa06f322dd5604f314b5f
Ahmed Alforjani <ahmedelforjani50@gmail.com>
2019-01-18 15:36:52 +0200

    Fixing code

--- nav.php
+++ nav.php
@@ -1,44 +1,4 @@
-<!doctype html>
-<!--[if IE 7 ]> <html lang="en" class="ie7"> <![endif]-->
-<!--[if IE 8 ]> <html lang="en" class="ie8"> <![endif]-->
-<!--[if IE 9 ]> <html lang="en" class="ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
-<html lang="en">
-<!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<meta name="description" content="">
-<meta name="author" content="">
-<link rel="shortcut icon" href="images/favicon.ico">
-<title>Automotive Car Dealership &amp; Business HTML Template</title>
-<!-- Bootstrap core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
 
-<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
-
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yellowtail%7COpen%20Sans%3A400%2C300%2C600%2C700%2C800" media="screen" />
-<!-- Custom styles for this template -->
-<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
-<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/jquery.bxslider.css" type="text/css" media="screen" />
-<link href="css/jquery.fancybox.css" rel="stylesheet">
-<link href="css/jquery.selectbox.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-<link href="css/mobile.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
-<link href="css/animate.min.css" rel="stylesheet">
-<link href="css/ts.css" type="text/css" rel="stylesheet">
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery.themepunch.tools.min.js"></script>
-<script type="text/javascript" src="js/jquery.themepunch.revolution.min.js"></script>
-<script type="text/javascript" src="js/wow.min.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key&amp;sensor=false"></script>
 
 <header  data-spy="affix" data-offset-top="1" class="clearfix">
     <!-- <section class="toolbar">
@@ -68,16 +28,16 @@
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                        <a class="navbar-brand" href="index.html"><span class="logo"><span class="primary_text">Automotive</span> <span class="secondary_text">Template</span></span></a> </div>
+                        <a class="navbar-brand" href="index.html"><span class="logo"><span class="primary_text">My Car</span> <span class="secondary_text">Station</span></span></a> </div>
                     
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav pull-right">
-                            <li class="active"><a href="index.php">Home</a></li>
-                            <li class=""><a href="inventory-boxed-fullwidth.php">Inventory</a></li>
-                            <li class=""><a href="blog.php">News</a></li>
-                            <li><a href="our-team.php">About Us</a></li>                            
-                            <li><a href="contact.php">Contact</a></li>
+                            <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active' ?>"><a href="index.php">Home</a></li>
+                            <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'inventory.php' || basename($_SERVER['PHP_SELF']) == 'car.php') echo 'active' ?>"><a href="inventory.php">Inventory</a></li>
+                            <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'blog.php') echo 'active' ?>"><a href="blog.php">News</a></li>
+                            <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'our-team.php') echo 'active' ?>"><a href="our-team.php">About Us</a></li>                            
+                            <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'contact.php') echo 'active' ?>"><a href="contact.php">Contact</a></li>
                         </ul>
                     </div>
                     <!-- /.navbar-collapse --> 
@@ -88,15 +48,3 @@
         <div class="header_shadow"></div>
     </div>
 </header>
-<script src="js/retina.js"></script> 
-<script type="text/javascript" src="js/jquery.parallax.js"></script> 
-<script type="text/javascript" src="js/jquery.inview.min.js"></script> 
-<script src="js/main.js"></script> 
-<script type="text/javascript" src="js/jquery.fancybox.js"></script> 
-<script src="js/modernizr.custom.js"></script> 
-<script defer src="js/jquery.flexslider.js"></script> 
-<script src="js/jquery.bxslider.js" type="text/javascript"></script> 
-<script src="js/jquery.selectbox-0.2.js" type="text/javascript"></script> 
-<script type="text/javascript" src="js/jquery.mousewheel.js"></script> 
-<script type="text/javascript" src="js/jquery.easing.js"></script>
-</html>
